8e8427280cda653d45591e5b7124f8acd25b6607
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-06-19 15:13:26 -0300

    lerp mvp

--- src/Mapbox/MapRoute.tsx
+++ src/Mapbox/MapRoute.tsx
@@ -2,7 +2,11 @@ import {gpx} from '@tmcw/togeojson'
 import deepClone from 'deep-clone'
 import {FeatureCollection} from 'geojson'
 import MapboxGL from 'mapbox-gl'
+import {Vector3} from 'three'
 import {DOMParser} from 'xmldom'
+
+const prevCameraPos = new Vector3()
+const nextCameraPos = new Vector3()
 
 export class MapRoute {
   map: MapboxGL.Map
@@ -41,11 +45,31 @@ export class MapRoute {
     })
   }
 
-  lookAt(lng: number, lat: number) {
+  lookAt(lng: number, lat: number, alpha = 1) {
     const camera = this.map.getFreeCameraOptions()
-    camera.position = MapboxGL.MercatorCoordinate.fromLngLat(
+    const nextCameraCoord = MapboxGL.MercatorCoordinate.fromLngLat(
         {lng: lng + this.cameraOffset, lat: lat + this.cameraOffset},
         this.altitude,
+    )
+    nextCameraPos.set(
+        nextCameraCoord.x,
+        nextCameraCoord.y,
+        nextCameraCoord.z || 0,
+    )
+    if (camera.position) {
+      prevCameraPos.set(
+          camera.position.x,
+          camera.position.y,
+          camera.position.z || 0,
+      )
+    } else {
+      prevCameraPos.copy(nextCameraPos)
+    }
+    const newCameraPos = prevCameraPos.lerp(nextCameraPos, alpha)
+    camera.position = new MapboxGL.MercatorCoordinate(
+        newCameraPos.x,
+        newCameraPos.y,
+        newCameraPos.z,
     )
     camera.lookAtPoint({lng, lat})
     this.map.setFreeCameraOptions(camera)
@@ -61,7 +85,7 @@ export class MapRoute {
       this.curGeojson.features[0].geometry.coordinates.push(curCoordinate)
       // @ts-expect-error -- TODO
       this.map.getSource('running-routes').setData(this.curGeojson)
-      this.lookAt(curCoordinate[0], curCoordinate[1])
+      this.lookAt(curCoordinate[0], curCoordinate[1], this.curIndex ? 1 : 1)
       this.curIndex++
     }
   }
