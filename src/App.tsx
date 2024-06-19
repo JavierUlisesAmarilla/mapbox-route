ab79ebd27c580969ed089114c72845740f263379
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-06-18 23:42:13 -0300

    gpx viewer mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import {gpx} from '@tmcw/togeojson'
 import MapboxGL from 'mapbox-gl'
 import {useEffect} from 'react'
+import {DOMParser} from 'xmldom'
+
+import {COL_DE_BRAUS_FROM_LUCERAM} from './asset/gpx'
 
 MapboxGL.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
 
 export const App = () => {
   useEffect(() => {
-    new MapboxGL.Map({
+    const parsedGPX = new DOMParser().parseFromString(
+        COL_DE_BRAUS_FROM_LUCERAM,
+    )
+    const geojson = gpx(parsedGPX)
+    const map = new MapboxGL.Map({
       container: 'mapbox',
       style: 'mapbox://styles/mapbox/dark-v11',
-      center: [-96, 37.8],
-      zoom: 3,
+      center: [7.361653, 43.8843],
+      zoom: 100,
       pitch: 40,
+    })
+
+    map.on('load', () => {
+      map.addSource('running-routes', {
+        type: 'geojson',
+        data: geojson,
+      })
+
+      map.addLayer({
+        id: 'running-routes-line',
+        type: 'line',
+        source: 'running-routes',
+        paint: {
+          'line-color': '#15cc09',
+          'line-width': 4,
+        },
+      })
     })
   }, [])
 
